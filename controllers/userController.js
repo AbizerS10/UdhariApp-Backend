c22a86075108a24a672f14eca94bba94fe0cd2ac
AbizerS10 <amdesigners52@gmail.com>
2023-02-06 10:39:36 +0530

    minor

--- controllers/userController.js
+++ controllers/userController.js
@@ -138,9 +138,9 @@ const userCtrl = {
         email,
         `
       <div style="max-width: 700px; margin: auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 1rem;">
-      <h2 style="text-align: center; text-transform: uppercase; color: teal;">Welcome to the Udhaari Alert App</h2>
-      <p>Congratulations! You're almost set to start using our Udhaari alert app.
-          Just click the button below to validate your email address.
+      <h2 style="text-align: center; text-transform: uppercase; color: teal;">Udhari Management App</h2>
+      <p>
+          Click the button below to reset your password.
       </p>
       <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display:inline-block;">Reset your password</a>
       <p>If the button doesn't work for any reason, you can also click on the link below:</p>
